643dacd12e6778758715f4deddba253afe96d92d
rajendarreddy jagapathi <rajendarreddyj@gmail.com>
2016-09-26 23:53:33 -0500

    updating script for vbox ext pack

--- ubuntu/install_virtualbox.sh
+++ ubuntu/install_virtualbox.sh
@@ -31,4 +31,20 @@ sudo usermod -a -G vboxusers $(whoami)
 # To access Shared Folder from VirtualBox guests, add users to vboxsf
 sudo usermod -a -G vboxsf $(whoami)
 
+# Install Extension pack
+echo "Installing virtualbox Extension pack"
+version=$(vboxmanage -v)
+echo $version
+var1=$(echo $version | cut -d 'r' -f 1)
+echo $var1
+var2=$(echo $version | cut -d 'r' -f 2)
+echo $var2
+file="Oracle_VM_VirtualBox_Extension_Pack-$var1-$var2.vbox-extpack"
+echo $file
+wget http://download.virtualbox.org/virtualbox/$var1/$file -O /tmp/$file
+#sudo VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
+sudo VBoxManage extpack install /tmp/$file --replace
+# Remove tmp file
+sudo rm /tmp/$file
+
 echo "virtualbox Installation Done"
