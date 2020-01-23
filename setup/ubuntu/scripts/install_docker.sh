751585907b5ec13aca30b0d7728b7dd20d903d2c
Rajendarreddy Jagapathi <rajendarreddyj@users.noreply.github.com>
2020-01-23 12:05:50 -0600

    Update install_docker.sh

--- setup/ubuntu/scripts/install_docker.sh
+++ setup/ubuntu/scripts/install_docker.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-# https://docs.docker.com/engine/installation/linux/ubuntulinux/
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
 
-echo "docker-engine Starting installation"
+echo "Docker Engine - Community Starting installation"
 
+echo "Installing recommended packages"
+sudo apt-get install  -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+    linux-image-extra-virtual 
+    
 echo "Adding docker-engine GPG Key"
 # Add the GPG key for the official Docker repository to the system
-# sudo apt-key adv --kesudo yserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-# sudo apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "Verify Fingerpring"
+sudo apt-key fingerprint 0EBFCD88
 
 echo "Adding docker-engine Repository"
 # Add the Docker repository to APT sources
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list'
-
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
 echo "Updating packages"
 # Update your package manager & Upgrade.
 sudo apt update && sudo apt -y upgrade
 
-echo "Installing recommended packages"
-# Install the recommended packages.
-sudo apt instay linux-i-y mage-extra-$(uname -r) linux-image-extra-virtual
-#  ensure that APT works with the https method
-sudo apt install -y apt-transport-https ca-certificates
-
-
-echo "Installing docker-engine pac
-kage"
-# Install docker-enginer.
-sudo apt install -y docker-engine
+echo "Installing docker-ce package"
+# Install docker-ce.
+sudo apt install -y install docker-ce docker-ce-cli containerd.io
 
 echo "Checking docker Status"
 # Docker should now be installed, the daemon started, and the process enabled to start on boot. Check that it's running:
@@ -39,4 +43,7 @@ echo "Adding username to docker group"
 # add your username to the docker group
 sudo usermod -aG docker $(whoami)
 
-echo "docker-engine Installation Done"
+# Verify that Docker CE is installed correctly by running the hello-world image.
+sudo docker run hello-world
+
+echo "Docker Engine - Community Installation Done"
