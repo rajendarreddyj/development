12a8421fdaa19050ce79e711a7d51fe345a3890e
Rajendarreddy Jagapathi <rajendarreddyj@users.noreply.github.com>
2020-01-23 12:13:28 -0600

    Update install_java.sh

--- setup/ubuntu/scripts/install_java.sh
+++ setup/ubuntu/scripts/install_java.sh
@@ -8,18 +8,12 @@ echo "Intall java repository dependency"
 # add-apt-repository dependency
 sudo apt -y install python3-software-properties
 
-echo "Adding java repository"
-# Add Java Repository
-sudo add-apt-repository -y ppa:webupd8team/java
-
 echo "Updating packages"
 # Update 
 sudo apt update && sudo apt -y upgrade
 
-echo "Installing java 8"
-# Install Java 8
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt install -y oracle-java8-installer
+echo "Installing java 11"
+# Install Java 11
+sudo apt install -y openjdk-11-jre-headless
 
 echo "java Installation Done"
