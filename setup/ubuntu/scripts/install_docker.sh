6689544d72b9fcd3a2e3bf199ac07dcd17cdf44b
Rajendarreddy Jagapathi <rajendarreddyj@users.noreply.github.com>
2020-01-25 15:12:17 -0600

    Update install_docker.sh

--- setup/ubuntu/scripts/install_docker.sh
+++ setup/ubuntu/scripts/install_docker.sh
@@ -47,4 +47,15 @@ sudo usermod -aG docker $(whoami)
 # Verify that Docker CE is installed correctly by running the hello-world image.
 sudo docker run hello-world
 
+#To create the docker group and add your user:
+#Create the docker group.
+sudo groupadd docker
+#Add your user to the docker group.
+sudo usermod -aG docker $USER
+#Log out and log back in so that your group membership is re-evaluated.
+#If testing on a virtual machine, it may be necessary to restart the virtual machine for changes to take effect.
+#On a desktop Linux environment such as X Windows, log out of your session completely and then log back in.
+#On Linux, you can also run the following command to activate the changes to groups:
+newgrp docker 
+
 echo "Docker Engine - Community Installation Done"
